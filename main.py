a45494076eeacd06c11b51d9430fd40fb90734fc
andrey-korol <andrey.korol@justeattakeaway.com>
2026-02-24 23:05:12 +0100

    Reflection agent

--- main.py
+++ main.py
@@ -1,39 +1,66 @@
-from dotenv import load_dotenv
-import os
-from langchain_core.prompts import PromptTemplate  
-from langchain_openai import ChatOpenAI
-from langchain_ollama import ChatOllama
+from typing import TypedDict, Annotated
 
+from dotenv import load_dotenv
 
 load_dotenv()
 
+from langchain_core.messages import BaseMessage, HumanMessage
+from langgraph.graph import END, StateGraph
+from langgraph.graph.message import add_messages
 
-def main():
-    information = """
-    Elon Reeve Musk (/ˈiːlɒn/ EE-lon; born June 28, 1971) is a businessman and entrepreneur known for his leadership of Tesla, SpaceX, Twitter, and xAI. Musk has been the wealthiest person in the world since 2025; as of February 2026, Forbes estimates his net worth to be around US$852 billion.
+from chains import generate_chain, reflect_chain
 
-Born into a wealthy family in Pretoria, South Africa, Musk emigrated in 1989 to Canada; he has Canadian citizenship since his mother was born there. He received bachelor's degrees in 1997 from the University of Pennsylvania before moving to California to pursue business ventures. In 1995, Musk co-founded the software company Zip2. Following its sale in 1999, he co-founded X.com, an online payment company that later merged to form PayPal, which was acquired by eBay in 2002. Musk also became an American citizen in 2002.
+class MessageGraph(TypedDict):
+    messages: Annotated[list[BaseMessage], add_messages]
 
-In 2002, Musk founded the space technology company SpaceX, becoming its CEO and chief engineer; the company has since led innovations in reusable rockets and commercial spaceflight. Musk joined the automaker Tesla as an early investor in 2004 and became its CEO and product architect in 2008; it has since become a leader in electric vehicles. In 2015, he co-founded OpenAI to advance artificial intelligence (AI) research, but later left; growing discontent with the organization's direction and their leadership in the AI boom in the 2020s led him to establish xAI, which became a subsidiary of SpaceX in 2026. In 2022, he acquired the social network Twitter, implementing significant changes, and rebranding it as X in 2023. His other businesses include the neurotechnology company Neuralink, which he co-founded in 2016, and the tunneling company the Boring Company, which he founded in 2017. In November 2025, a Tesla pay package worth $1 trillion for Musk was approved, which he is to receive over 10 years if he meets specific goals.
-    """
-    
-    system_prompt = f"""given the following {information} i want to create :
-    1. a short summary of the information
-    2. interesting facts about the information
-    """
-    
-    prompt = PromptTemplate(
-        template=system_prompt,
-        input_variables=["information"]
-    )
-    
-    #llm=ChatOpenAI(temperature=0, model="gpt-5")
-    llm = ChatOllama(model="llama3.1:8b")
-    
-    chain = prompt | llm
-    chain_output = chain.invoke(input={"information": information})
-    
-    print(chain_output.content)
+
+REFLECT = "reflect"
+GENERATE = "generate"
+
+
+def generation_node(state: MessageGraph):
+    return {"messages": [generate_chain.invoke({"messages": state["messages"]})]}
+
+
+def reflection_node(state: MessageGraph):
+    res = reflect_chain.invoke({"messages": state["messages"]})
+    return {"messages": [HumanMessage(content=res.content)]}
+
+
+builder = StateGraph(state_schema=MessageGraph)
+builder.add_node(GENERATE, generation_node)
+builder.add_node(REFLECT, reflection_node)
+builder.set_entry_point(GENERATE)
+
+
+def should_continue(state: MessageGraph):
+    if len(state["messages"]) > 6:
+        return END
+    return REFLECT
+
+builder.add_conditional_edges(GENERATE, should_continue, path_map={END:END, REFLECT: REFLECT})
+builder.add_edge(REFLECT, GENERATE)
+
+graph = builder.compile()
+print(graph.get_graph().draw_mermaid())
+graph.get_graph().print_ascii()
 
 if __name__ == "__main__":
-    main()
+    print("Hello LangGraph")
+    inputs = {
+        "messages": [
+            HumanMessage(
+                content="""Make this tweet better:"
+                                    @LangChainAI
+            — newly Tool Calling feature is seriously underrated.
+
+            After a long wait, it's  here- making the implementation of agents across different models with function calling - super easy.
+
+            Made a video covering their newest blog post
+
+                                  """
+            )
+        ]
+    }
+    response = graph.invoke(inputs)
+    print(response)
